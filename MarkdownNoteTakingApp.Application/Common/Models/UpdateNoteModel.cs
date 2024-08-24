6b292c13bcbdbb72d7e8177d380fd78d86f04e1b
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 20:13:15 +0500

    Infrastructure : adding migrations for update "Note" entity; Application : Common/Models : create UpdateNoteModel (Id, Title, Content, File);

---
+++ MarkdownNoteTakingApp.Application/Common/Models/UpdateNoteModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MarkdownNoteTakingApp.Application.Common.Models
+{
+    public class UpdateNoteModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        // for file 
+        public IFormFile? File { get; set; }
+    }
+}
---
+++ MarkdownNoteTakingApp.Infrastructure/Data/Migrations/20240824150927_addingFileFields.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarkdownNoteTakingApp.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingFileFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "Notes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Notes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "FileSize",
+                table: "Notes",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "FileSize",
+                table: "Notes");
+        }
+    }
+}
