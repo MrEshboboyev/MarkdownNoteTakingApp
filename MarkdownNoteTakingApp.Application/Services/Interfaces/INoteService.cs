bfab464e7e110098590f288a689a996124a371f9
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 14:31:17 +0500

    Application : Package reference Domain; Create Services/Interfaces/INoteService.cs : Create, GetById, Delete, GetAll, Render, Check methods of Note;

---
+++ MarkdownNoteTakingApp.Application/Services/Interfaces/INoteService.cs
@@ -0,0 +1,14 @@
+﻿using MarkdownNoteTakingApp.Domain.Entities;
+
+namespace MarkdownNoteTakingApp.Application.Services.Interfaces
+{
+    public interface INoteService
+    {
+        Task<Guid> CreateNoteAsync(string title, string content);
+        Task<Note> GetNoteByIdAsync(Guid id);
+        Task<IEnumerable<Note>> GetAllNotesAsync();
+        Task DeleteNoteAsync(Guid id);
+        Task<string> RenderNoteToHtmlAsync(Guid id);
+        Task<string> CheckNoteGrammarAsync(Guid id);
+    }
+}
