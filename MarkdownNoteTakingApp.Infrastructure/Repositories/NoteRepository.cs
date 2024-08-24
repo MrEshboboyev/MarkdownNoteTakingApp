4293c60c2152b6554d7fce14cd4d504e93bbe1ba
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 14:50:30 +0500

    Infrastructure : Added project reference Application; Create Repositories/NoteRepository.cs : inherit from INoteRepository, inject DI, methods implemented;

---
+++ MarkdownNoteTakingApp.Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,45 @@
+﻿using MarkdownNoteTakingApp.Application.Common.Interfaces;
+using MarkdownNoteTakingApp.Domain.Entities;
+using MarkdownNoteTakingApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarkdownNoteTakingApp.Infrastructure.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        // inject Database
+        private readonly ApplicationDbContext _db;
+
+        public NoteRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Guid> AddNoteAsync(Note note)
+        {
+            _db.Notes.Add(note);
+            await _db.SaveChangesAsync();
+            return note.Id;
+        }
+
+        public async Task DeleteNoteAsync(Guid id)
+        {
+            var note = _db.Notes.FirstOrDefault(x => x.Id == id);
+            if (note != null)
+            {
+                _db.Notes.Remove(note);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Note>> GetAllNotesAsync()
+        {
+            return await _db.Notes.ToListAsync();
+        }
+
+        public async Task<Note> GetNoteByIdAsync(Guid id)
+        {
+            return await _db.Notes.FindAsync(id);
+        }
+    }
+}
