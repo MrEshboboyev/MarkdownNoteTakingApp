0d70090f3ffd0db1b1c5a743495ab694e37eb2f7
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 14:17:37 +0500

    Domain : Create Entities/Note.cs -> Id, Title, Content, CreatedAt, LastModifiedAt;

---
+++ MarkdownNoteTakingApp.Domain/Entities/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkdownNoteTakingApp.Domain.Entities
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+    }
+}
