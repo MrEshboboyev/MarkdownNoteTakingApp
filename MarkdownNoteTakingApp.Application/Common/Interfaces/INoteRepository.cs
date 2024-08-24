b72e3a9761afabaac2fb56a774a9a300441d312d
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 14:35:22 +0500

    Application/Common/Interfaces : Create INoteRepository.cs : Add, Delete, GetById, GetAll methods of Notes;

---
+++ MarkdownNoteTakingApp.Application/Common/Interfaces/INoteRepository.cs
@@ -0,0 +1,12 @@
+﻿using MarkdownNoteTakingApp.Domain.Entities;
+
+namespace MarkdownNoteTakingApp.Application.Common.Interfaces
+{
+    public interface INoteRepository
+    {
+        Task<Guid> AddNoteAsync(Note note);
+        Task<Note> GetNoteByIdAsync(Guid id);
+        Task<IEnumerable<Note>> GetAllNotesAsync();
+        Task DeleteNoteAsync(Guid id);
+    }
+}
