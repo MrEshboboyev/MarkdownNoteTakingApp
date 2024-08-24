8e180230baf62851338f2766710b55d8df8bc059
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 16:02:05 +0500

    Application : Services/Implementation : create NoteService.cs : inherit from INoteService, inject (INoteRepository, IGrammarCheckService, IMarkdownRenderService), methods implemented;

---
+++ MarkdownNoteTakingApp.Application/Services/Implementation/NoteService.cs
@@ -0,0 +1,64 @@
+﻿using MarkdownNoteTakingApp.Application.Common.Interfaces;
+using MarkdownNoteTakingApp.Application.Services.Interfaces;
+using MarkdownNoteTakingApp.Domain.Entities;
+
+namespace MarkdownNoteTakingApp.Application.Services.Implementation
+{
+    public class NoteService : INoteService
+    {
+        // inject INoteRepository, IGrammarCheckService, IMarkdownRenderService 
+        private readonly INoteRepository _noteRepository;
+        private readonly IGrammarCheckService _grammarCheckService;
+        private readonly IMarkdownRenderService _markdownRenderService;
+
+        public NoteService(INoteRepository noteRepository, 
+            IGrammarCheckService grammarCheckService,
+            IMarkdownRenderService markdownRenderService)
+        {
+            _noteRepository = noteRepository;
+            _grammarCheckService = grammarCheckService;
+            _markdownRenderService = markdownRenderService;
+        }
+
+        public async Task<string> CheckNoteGrammarAsync(Guid id)
+        {
+            var note = await _noteRepository.GetNoteByIdAsync(id);
+            return await _grammarCheckService.CheckGrammarAsync(note.Content);
+        }
+
+        public async Task<Guid> CreateNoteAsync(string title, string content)
+        {
+            var note = new Note 
+            {
+                Id = Guid.NewGuid(),
+                Title = title, 
+                Content = content,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await _noteRepository.AddNoteAsync(note);
+            return note.Id;
+        }
+
+        public async Task DeleteNoteAsync(Guid id)
+        {
+            await _noteRepository.DeleteNoteAsync(id);
+        }
+
+        public async Task<IEnumerable<Note>> GetAllNotesAsync()
+        {
+            return await _noteRepository.GetAllNotesAsync();
+        }
+
+        public async Task<Note> GetNoteByIdAsync(Guid id)
+        {
+            return await _noteRepository.GetNoteByIdAsync(id);
+        }
+
+        public async Task<string> RenderNoteToHtmlAsync(Guid id)
+        {
+            var note = await _noteRepository.GetNoteByIdAsync(id);
+            return await _markdownRenderService.RenderMarkdownToHtmlAsync(note.Content);
+        }
+    }
+}
