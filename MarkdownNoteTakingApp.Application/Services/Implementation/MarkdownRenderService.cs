bd0a0c75bbff288f14f10fe38247a60478b5c33f
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 15:54:42 +0500

    Application : Services/Implementation : create MarkdownRenderService : inherit from IMarkdownRenderService, method implemented; Adding package : Markdig;

---
+++ MarkdownNoteTakingApp.Application/Services/Implementation/MarkdownRenderService.cs
@@ -0,0 +1,16 @@
+﻿using Markdig;
+using MarkdownNoteTakingApp.Application.Services.Interfaces;
+
+namespace MarkdownNoteTakingApp.Application.Services.Implementation
+{
+    public class MarkdownRenderService : IMarkdownRenderService
+    {
+        public async Task<string> RenderMarkdownToHtmlAsync(string markdownContent)
+        {
+            // Use MarkDig to convert MarkDown to Html
+            var pipeline = new MarkdownPipelineBuilder().Build();
+            var html = Markdown.ToHtml(markdownContent, pipeline);
+            return await Task.FromResult(html);
+        }
+    }
+}
