f5ea3e63a37adf88b7c76261355f86dbdc48a3a5
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 14:43:48 +0500

    Adding Database connections and configuring, adding migrations;

---
+++ MarkdownNoteTakingApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MarkdownNoteTakingApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarkdownNoteTakingApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
