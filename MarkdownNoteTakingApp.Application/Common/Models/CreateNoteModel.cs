5f96dc1ec066a4bbae15e6143602e95dcc74995a
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-24 20:05:16 +0500

    Domain : Note.cs : added fields (FileName, FilePath, FileSize); Application : Package added : Microsoft.AspNetCore.Http.Features; Common/Models/CreateNoteModel.cs : added IFormFile? File;

--- MarkdownNoteTakingApp.Application/Common/Models/CreateNoteModel.cs
+++ MarkdownNoteTakingApp.Application/Common/Models/CreateNoteModel.cs
@@ -1,8 +1,13 @@
-﻿namespace MarkdownNoteTakingApp.Application.Common.Models
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MarkdownNoteTakingApp.Application.Common.Models
 {
     public class CreateNoteModel
     {
         public string Title { get; set; }
         public string Content { get; set; }
+
+        // for file 
+        public IFormFile? File { get; set; }
     }
 }
--- MarkdownNoteTakingApp.Domain/Entities/Note.cs
+++ MarkdownNoteTakingApp.Domain/Entities/Note.cs
@@ -7,5 +7,10 @@
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? LastModifiedAt { get; set; }
+
+        // New properties to store file information
+        public string FileName { get; set; }    
+        public string FilePath { get; set; }    
+        public long? FileSize { get; set; }    
     }
 }
